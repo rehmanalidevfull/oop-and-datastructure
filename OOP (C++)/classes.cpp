877dd8d7455c178af1e55d9c75f34e23efcbcf7b
Rehman Ali <chilldudeexplanations24@gmail.com>
2026-09-18 20:20:55 +0500

    Add files via upload

---
+++ OOP (C++)/classes.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// Classes are the blue print of an entity. it explain or tell what an object has and what it can perfom.
+
+class HelloWorld{
+private:
+    string contact = "0321-1471124";
+public:
+    string name = "Rehman ALi";
+    string rollno = "BSSE-E2-55";
+    string className = "SE";
+
+    // action 
+    void showDetail(){
+        cout<<"Name: "<<this->name<<endl;
+        cout<<"Class: "<<this->className<<endl;
+        cout<<"RollNo: "<<this->rollno<<endl;
+        cout<<"Contact: "<<this->contact<<endl;
+    }
+    void changename(string name){
+        this->name = name;
+        cout<<"Name Changed to "<< name<< endl;
+    }
+    void setContact(string cont){
+        this->contact = cont;
+    }
+};
+
+int main(){
+    // creating object of class
+    HelloWorld obj;
+    obj.showDetail();
+    obj.changename("Ali");
+    obj.showDetail();
+    return 0;
+}
